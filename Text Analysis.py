1501ca41de0a2a74d349f6d5735d96e4c61323a3
Amey <96690322+Coding-Storm@users.noreply.github.com>
2022-09-06 13:56:44 +0530

    Update Text Analysis.py

--- Text Analysis.py
+++ Text Analysis.py
@@ -1,16 +1,16 @@
 from textblob import TextBlob
 
-text = ['Idiot','Smart']
+texts = ['Idiot','Smart']
 
 non_toxic = []
 toxic = []
 
-for texts in text:
+for text in texts:
     text_polarity = TextBlob(text).sentiment.polarity
     if text_polarity > 0:
         non_toxic.append(text)
         continue
     toxic.append(text)
-
+print("Made by Coding-Storm")
 print('Non toxic : ',non_toxic)
 print('Toxic : ',toxic)
